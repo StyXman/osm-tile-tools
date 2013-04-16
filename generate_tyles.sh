6f63828e9053e0f43e0ca5257a807f509a08f300
Marcos Dione <mdione@grulic.org.ar>
2013-04-16 22:00:55 +0200

    + pass options to the python script.

--- generate_tyles.sh
+++ generate_tyles.sh
@@ -3,4 +3,4 @@
 export MAPNIK_TILE_DIR=tiles
 export MAPNIK_MAP_FILE=tilemill/project/osm-tilemill/mapnik.xml
 
-python mapnik-stylesheets/generate_tiles.py
+python mapnik-stylesheets/generate_tiles.py "$@"
