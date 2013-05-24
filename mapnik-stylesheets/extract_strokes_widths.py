712547e3c7b954893ce7ea0025a8671433dd5043
Marcos Dione <mdione@grulic.org.ar>
2013-05-24 13:17:30 +0200

    * have a list of the styles to modify; ignore the rest. + extract zoom levels form inc/entities.xml.inc. * better name normalization. + also generate entities for the widths.

--- mapnik-stylesheets/extract_strokes_widths.py
+++ mapnik-stylesheets/extract_strokes_widths.py
@@ -3,29 +3,76 @@
 from lxml import etree
 import sys
 from collections import defaultdict
+import re
+
+# read inc/entities.xml.inc to capture the scale denominators
+mn_zooms= {None: 0}
+mx_zooms= {}
+mn= None
+mx= None
+
+f= open ('inc/entities.xml.inc')
+#<!ENTITY maxscale_zoom1 "<MaxScaleDenominator>500000000</MaxScaleDenominator>">
+#<!ENTITY minscale_zoom1 "<MinScaleDenominator>200000000</MinScaleDenominator>">
+#<!ENTITY minscale_zoom18 "">
+min_max_parser= re.compile ('<!ENTITY (min|max)scale_zoom(\d+) "(<(Min|Max)ScaleDenominator>(\d+)</.*>)?">')
+for line in f:
+    g= min_max_parser.search (line)
+    if g is not None:
+        what= g.group (1)
+        zoom= g.group (2)
+        scale= g.group (5)
+
+        if what=='max':
+            mx_zooms[scale]= zoom
+        else:
+            mn_zooms[scale]= zoom
+f.close ()
 
 parser = etree.XMLParser(resolve_entities=False)
 
 tree= etree.parse (sys.argv[1], parser=parser)
+
+style_names= (
+    'highway-area-casing',
+    'highway-area-fill',
+    # 'highway-junctions', these are just text
+    'tunnels-casing',
+    'tunnels-fill',
+    'minor-roads-casing-links',
+    'minor-roads-casing',
+    'minor-roads-fill-links',
+    'minor-roads-fill',
+    'footbikecycle-tunnels',
+    'tracks-notunnel-nobridge',
+    'tracks-tunnels',
+    'waterway-bridges',
+    'bridges_casing',
+    'bridges_casing2',
+    'bridges_fill',
+    'roads',
+    'trams',
+    'guideways',
+    )
 
 # ignore the first definition
 ignore_first= (
     )
 # ignore definitions starting from the second one
 ignore_others= (
-    'minor-roads-casing_a_highway__is__residential_or_highway__is__unclassified_or_highway__is__road_z_and_not_tunnel_is_yes',
-    'minor-roads-fill_highway__is__steps',
-    'guideways_railway__is__tram_and_bridge_is_yes',
+    'minor-roads-casing_a_highway_is_residential_or_highway_is_unclassified_or_highway_is_road_z_and_not_tunnel_is_yes',
+    'minor-roads-fill_highway_is_steps',
+    'guideways_railway_is_tram_and_bridge_is_yes',
     )
 # completely ignore
 ignore= (
-    'minor-roads-fill_railway__is__rail_and_tunnel__is__yes',
-    'minor-roads-fill_railway__is__rail_and_not_tunnel__is__yes',
-    'minor-roads-fill_railway__is__spurminus_sidingminus_yard_and_tunnel__is__yes',
-    'minor-roads-fill_railway__is__spurminus_sidingminus_yard_and_not_tunnel__is__yes',
+    'minor-roads-fill_railway_is_rail_and_tunnel_is_yes',
+    'minor-roads-fill_railway_is_rail_and_not_tunnel_is_yes',
+    'minor-roads-fill_railway_is_spurminus_sidingminus_yard_and_tunnel_is_yes',
+    'minor-roads-fill_railway_is_spurminus_sidingminus_yard_and_not_tunnel_is_yes',
     'minor-roads-fill_a_railway_is_narrow_gauge_or_railway_is_funicular_z_and_tunnel_is_yes',
-    'minor-roads-fill_a_highway__is__cycleway_or_a_highway__is__path_and_bicycle__is__designated_z_z_and_not_tunnel__is__yes',
-    'minor-roads-fill_highway__is__platform_or_railway__is__platform',
+    'minor-roads-fill_a_highway_is_cycleway_or_a_highway_is_path_and_bicycle_is_designated_z_z_and_not_tunnel_is_yes',
+    'minor-roads-fill_highway_is_platform_or_railway_is_platform',
     )
 
 color_map= {
@@ -58,8 +105,21 @@ color_map= {
 # map
 root= tree.getroot ()
 
-entities= {}
+color_entities= {}
+size_entities= {}
 colors= defaultdict (list)
+
+def sanitize (s):
+    return ''.join ([ c for c  in s.replace ('!=', '_isnot_').
+                                    replace ('=', '_is_').
+                                    replace ("''", 'empty').
+                                    replace ('>', '_gt_').
+                                    replace ('<', '_lt_').
+                                    replace (' ', '_').
+                                    replace ('(', 'q_').
+                                    replace (')', '_p').
+                                    replace ('-', '_minus_')
+                                if c not in "'[]" ]).replace ('__', '_')
 
 for level1 in root.iter ('Style'):
     try:
@@ -67,65 +127,79 @@ for level1 in root.iter ('Style'):
     except AttibuteError, e:
         print 'malformed style?', e
     else:
-        for level2 in level1.iter ('Rule'):
-            #<Rule>
-               #<Filter>[highway] = 'pedestrian' or [highway]='service' or [highway]='footway' or [highway]='path'</Filter>
-               #&maxscale_zoom14;
-               #<LineSymbolizer stroke="grey" stroke-width="1"/>
-            #</Rule>
-            for level3 in level2.iter ('Filter'):
-                # actually there should be only one
-                # 'tertiary_link' and not [tunnel]='yes'
-                suffix= level3.text
+        if style in style_names:
+            for level2 in level1.iter ('Rule'):
+                #<Rule>
+                  #<Filter>[highway] = 'motorway' or [highway]='motorway_link'</Filter>
+                  #<MaxScaleDenominator>200000</MaxScaleDenominator>
+                  #<MinScaleDenominator>100000</MinScaleDenominator>
+                  #<LineSymbolizer stroke="#506077" stroke-width="3" stroke-dasharray="4,2"/>
+                #</Rule>
+                for level3 in level2.iter ('Filter'):
+                    # actually there should be only one
+                    # [highway] = 'motorway' or [highway]='motorway_link'
+                    suffix= level3.text
 
-            for level3 in level2.iter ('LineSymbolizer'):
-                color= level3.attrib['stroke']
-                # 'escape' things in text
-                stroke_name= ''.join ( [ c  for c in suffix.replace ('!=', '_isnot_').
-                                                            replace ('=', '_is_').
-                                                            replace ("''", 'empty').
-                                                            replace ('>', '_gt_').
-                                                            replace ('<', '_lt_').
-                                                            replace (' ', '_').
-                                                            replace ('(', 'a_').
-                                                            replace (')', '_z').
-                                                            replace ('-', 'minus_')
-                                            if c not in "'[]" ] ).replace ('__', '_')
+                # find the scale denominators
+                mnzm= 0
+                mxzm= 18
+                # NOTE: these might seem inverted
+                # well, yes, they are :)
+                for level3 in level2.iter ('MaxScaleDenominator'):
+                    mnzm= mx_zooms[level3.text]
+                for level3 in level2.iter ('MinScaleDenominator'):
+                    mxzm= mn_zooms[level3.text]
+                
+                for level3 in level2.iter ('LineSymbolizer'):
+                    color= level3.attrib['stroke']
+                    # 'escape' things in text
+                    stroke_name= sanitize (suffix)
 
-                # <!ENTITY maxscale_zoom0 "<MaxScaleDenominator>250000000000</MaxScaleDenominator>">
-                entity= "%s_%s" % (style, stroke_name)
-                if not entity in ignore:
-                    level3.set ('stroke', "&%s;" % entity)
-                    old= entities.get (entity, None)
-                    if old is None:
-                        # first seen
-                        if not entity in ignore_first:
-                            entities[entity]= color
-                            # this is tricky: update the color with the new definition
-                            # or keep it as before
-                            colors[color_map.get (color, color)].append (entity)
-                        else:
-                            print "<!-- ignoring first %s: %s -->" % (entity, color)
-                            entities[entity]= 'seen'
-                    else:
-                        if not entity in ignore_others:
-                            if color!=old:
-                                entities[entity]= color
-                                colors[color_map.get (color, color)].append (entity)
+                    color_entity= "%s_%s" % (style, stroke_name)
+                    size_entity= "%s_%s-%s_%s" % (stroke_name, mnzm, mxzm, style)
+                    
+                    if not color_entity in ignore:
+                        level3.set ('stroke', "&%s;" % color_entity)
+                        old= color_entities.get (color_entity, None)
+                        if old is None:
+                            # first seen
+                            if not color_entity in ignore_first:
+                                color_entities[color_entity]= color
+                                # this is tricky: update the color with the new definition
+                                # or keep it as before
+                                colors[color_map.get (color, color)].append (color_entity)
+
                             else:
-                                # repeated
-                                pass
+                                print "<!-- ignoring first %s: %s -->" % (color_entity, color)
+                                color_entities[color_entity]= 'seen'
                         else:
-                            print "<!-- ignoring other %s: %s -->" % (entity, color)
-                else:
-                    print "<!-- ignoring %s: %s -->" % (entity, color)
+                            if not color_entity in ignore_others:
+                                if color!=old:
+                                    color_entities[color_entity]= color
+                                    colors[color_map.get (color, color)].append (color_entity)
+                                else:
+                                    # repeated
+                                    pass
+                            else:
+                                print "<!-- ignoring other %s: %s -->" % (color_entity, color)
+                                
+                        # handle sizes too
+                        size_entities[size_entity]= level3.attrib['stroke-width']
+                        level3.set ('stroke-width', "&%s;" % size_entity)
+                    else:
+                        print "<!-- ignoring %s: %s -->" % (color_entity, color)
+        else:
+            print "<!-- ignoring style %s -->" % (style)
 
 print
                     
 for color in sorted (colors.keys ()):
     print "<!-- %s -->" % color
-    for entity in colors[color]:
-        print '<!ENTITY %s "%s">' % (entity, color)
-    print 
+    for color_entity in colors[color]:
+        print '<!ENTITY %s "%s">' % (color_entity, color)
+    print
+
+for size_entity in sorted (size_entities.keys ()):
+    print '<!ENTITY %s "%s">' % (size_entity, size_entities[size_entity])
 
 tree.write (sys.argv[2])
