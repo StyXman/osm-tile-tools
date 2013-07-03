24a0b7a8251d1cbfb5561b2ab264756ffe278800
Marcos Dione <mdione@grulic.org.ar>
2013-07-03 21:23:52 +0200

    + script for handling marble's files.

---
+++ mapnik-stylesheets/change_marble_tile_dir.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+
+if [ $# -eq 1 ]; then
+    dst=$1
+    cp -Rv tiles/{legend*,Elevation.dgml,preview.png} $dst/
+else
+    dst=tiles
+fi
+
+src=$(pwd)/$dst
+dst=$HOME/.local/share/marble/maps/earth/Elevation
+rm -fv $dst
+ln -sv $src $dst 
