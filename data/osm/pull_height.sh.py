f1872aeaf215a4b45f4b5fae9221b13d566032a1
Marcos Dione <mdione@grulic.org.ar>
2013-07-20 16:27:18 +0200

    * use SRTMv4.1.

--- data/osm/pull_height.sh.py
+++ data/osm/pull_height.sh.py
@@ -4,15 +4,17 @@
 # no equivalent yet
 
 import sys
-import sh
+from sh import grep, osmpbf_outline, unzip, ErrorReturnCode
 import re
 import math
 from file_test import _f
 import time
 import os
+import urllib.request
+import urllib.error
 
 pbf_file= sys.argv[1]
-extent= sh.grep (sh.osmpbf_outline (pbf_file, _tty_out=False, _ok_code=1), 'bbox')
+extent= grep (osmpbf_outline (pbf_file, _tty_out=False, _ok_code=1), 'bbox')
 
 #     bbox: 9.5267800,46.3685100,17.1627300,49.0240300
 try:
@@ -23,10 +25,23 @@ except (ValueError, AttributeError) as e:
 
 print (west, south, east, north)
 
-# we don't need the +1 on the left right limit, as we're already using ceiling()
-# but the tiles count from the south and west
-for lat in range (math.floor (south), math.ceil (north)):
-    for lon in range (math.floor (west), math.ceil (east)):
+# we have 5x5 degrees sqaures that start at
+#  1, 1 for -180,+59,-179,+60
+# 13,37 for    0,  0,  +1, +1
+# 24,72 for +179,-60,+180,-59
+
+srtm_v41_w= math.floor (west/5)+37
+srtm_v41_s= math.floor (-south/5)+13
+srtm_v41_e= math.floor (east/5)+37
+srtm_v41_n= math.floor (-north/5)+13
+
+print (srtm_v41_w, srtm_v41_s, srtm_v41_e, srtm_v41_n)
+
+os.chdir ('../height')
+
+# +1 so they're proper bounds for range
+for lat in range (srtm_v41_n, srtm_v41_s+1):
+    for lon in range (srtm_v41_w, srtm_v41_e+1):
         if lat<0:
             lat_template= "S%02d"
         else:
@@ -37,19 +52,25 @@ for lat in range (math.floor (south), math.ceil (north)):
         else:
             lon_template= "E%03d"
 
-        zip_file= "%s%s.hgt.zip" % (lat_template % abs (lat), lon_template % abs (lon))
+        # zip_file= "%s%s.hgt.zip" % (lat_template % abs (lat), lon_template % abs (lon))
+        zip_file= "srtm_%02d_%02d.zip" % (lon, lat)
 
         if not _f (zip_file):
+            url= "http://srtm.csi.cgiar.org/SRT-ZIP/SRTM_V41/SRTM_Data_GeoTiff/%s" % zip_file
             try:
-                url= "http://dds.cr.usgs.gov/srtm/version2_1/SRTM3/South_America/%s" % zip_file
-                reason= sh.wget (url, _out=sys.stdout)
+                print ("Getting %s ... " % zip_file, end='', flush=True)
+                urllib.request.urlretrieve (url, zip_file)
+                print ("done!")
+            except urllib.error.HTTPError as e:
+                print (e)
 
-                print ("Got %s" % zip_file)
-            except sh.ErrorReturnCode as e:
-                print ("could not get %s; skipping" % url)
 
-        if _f(zip_file):
-            sh.unzip ("-u %s" % zip_file)
-            os.unlink (zip_file)
+        if _f (zip_file):
+            try:
+                # unzip (zip_file)
+                # os.unlink (zip_file)
+                pass
+            except ErrorReturnCode as e:
+                print ("unzipping %s failed; keeping..." % zip_file)
 
         time.sleep (1)
