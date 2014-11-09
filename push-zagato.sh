0f6e74939216a524e3eeca1f9eacccdb3f9ca109
Marcos Dione <mdione@grulic.org.ar>
2014-11-09 23:14:22 +0100

    [*] replace rsync by our own tool. because of the tool's limitation it properly adds x dirs, adds and removes y files, but doesn't remove x dirs.

--- push-zagato.sh
+++ push-zagato.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-tmp=$(tempfile -d . -s .lst)
-for i in "$@"; do
-    ./tile_list.py $(grep $1 bboxes.txt | cut -d '=' -f 2) >> $tmp
-done
-
-rsync --verbose --archive --update --inplace --delete --delete-during \
-    --ignore-missing-args --stats \
-    --files-from $tmp ./ "/media/sdc/local/share/marble/maps/earth/Elevation/"
+tmp=$(tempfile --directory . --suffix .lst)
+(
+    for i in "$@"; do
+        ./tile_list.py $(grep $1 bboxes.txt | cut --delimiter '=' --fields 2)
+    done
+) | sort --unique | tee $tmp | ./push-zagato.py \
+    "Elevation" "/media/mdione/Nokia N9/local/share/marble/maps/earth/Elevation"
 
 rm $tmp
