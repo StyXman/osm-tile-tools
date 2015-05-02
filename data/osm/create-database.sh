aa64d27a477d7b98499500491e96b8e313a5332a
Marcos Dione <mdione@grulic.org.ar>
2015-05-02 20:21:30 +0200

    [-] very ancient script.

--- data/osm/create-database.sh
+++
@@ -1,3 +0,0 @@
-psql --user postgres --command "create database osm;"
-psql --user postgres --file /usr/share/postgresql/9.1/contrib/postgis-1.5/postgis.sql
-psql --user postgres --file /usr/share/postgresql/9.1/contrib/postgis-1.5/spatial_ref_sys.sql
