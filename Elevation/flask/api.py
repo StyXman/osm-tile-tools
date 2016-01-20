b8d1b12e9a0d09c82cc713b1ac074ba8844f31df
Marcos Dione <mdione@grulic.org.ar>
2016-01-20 17:21:07 +0100

    [*] follow better RESTful convetion.

--- Elevation/flask/api.py
+++ Elevation/flask/api.py
@@ -5,6 +5,9 @@ from model import Trip, session
 app= Flask (__name__)
 api= Api (app)
 
+# Method  URI     Action
+# PUT     http://[hostname]/trips/[name]
+# DELETE  http://[hostname]/trips/[name]
 
 parser= reqparse.RequestParser ()
 parser.add_argument ('name')
@@ -12,11 +15,12 @@ parser.add_argument ('coord', type=float, action='append')
 
 class TripController (Resource):
     def get (self, name):
+        # GET     http://[hostname]/trips/[name]
         trip= session.query (Trip).filter_by (name=name).first ()
 
         return trip.toJson ()
 
-api.add_resource (TripController, '/trip/<name>')
+api.add_resource (TripController, '/trips/<name>')
 
 if __name__=='__main__':
     app.run (debug=True)
