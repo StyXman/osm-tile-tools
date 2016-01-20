746b1bce195adffaba67d74f5d10a0910b1f7eac
Marcos Dione <mdione@grulic.org.ar>
2016-01-20 17:21:48 +0100

    [+] trip listing support.

--- Elevation/flask/api.py
+++ Elevation/flask/api.py
@@ -20,7 +20,16 @@ class TripController (Resource):
 
         return trip.toJson ()
 
+# this is kinda silly
+class TripsController (Resource):
+
+    def get (self):
+        # GET     http://[hostname]/trips
+        trips= session.query (Trip).all ()
+        return [ trip.toJson () for trip in trips ]
+
 api.add_resource (TripController, '/trips/<name>')
+api.add_resource (TripsController, '/trips')
 
 if __name__=='__main__':
     app.run (debug=True)
