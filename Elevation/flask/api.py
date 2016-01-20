5ce302b0f59f4ea12edb5f751b4955ce9a23354a
Marcos Dione <mdione@grulic.org.ar>
2016-01-20 17:20:07 +0100

    [*] moved serialization code to the model.

--- Elevation/flask/api.py
+++ Elevation/flask/api.py
@@ -1,17 +1,10 @@
 from flask import Flask
 from flask_restful import reqparse, abort, Api, Resource
 from model import Trip, session
-import itertools
 
 app= Flask (__name__)
 api= Api (app)
 
-# taken from itertools' doc
-def grouper(iterable, n, fillvalue=None):
-    "Collect data into fixed-length chunks or blocks"
-    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
-    args = [iter(iterable)] * n
-    return itertools.zip_longest(*args, fillvalue=fillvalue)
 
 parser= reqparse.RequestParser ()
 parser.add_argument ('name')
@@ -20,9 +13,8 @@ parser.add_argument ('coord', type=float, action='append')
 class TripController (Resource):
     def get (self, name):
         trip= session.query (Trip).filter_by (name=name).first ()
-        points= [ (x, y) for x, y in grouper (trip.points, 2) ]
 
-        return { 'name': trip.name, 'points': points }
+        return trip.toJson ()
 
 api.add_resource (TripController, '/trip/<name>')
 
--- Elevation/flask/model.py
+++ Elevation/flask/model.py
@@ -3,10 +3,18 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy_utils import ScalarListType
+import itertools
 
 engine= create_engine ('sqlite:///trip_planner.sqlite3')
 
 Tables= declarative_base ()
+
+# taken from itertools' doc
+def grouper(iterable, n, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return itertools.zip_longest(*args, fillvalue=fillvalue)
 
 class Trip (Tables):
     __tablename__= 'trips'
@@ -14,6 +22,15 @@ class Trip (Tables):
     id= Column (Integer, primary_key=True)
     name= Column (String)
     points= Column (ScalarListType(float))
+
+    def toJson (self):
+        """Convert a Trip to a dict representing the trip.
+        Notice that the points is converted from a list of floats to alist of tuples of floats.
+        Notice too that it is not converted to a string."""
+
+        points= [ (x, y) for x, y in grouper (self.points, 2) ]
+
+        return { 'name': self.name, 'points': points }
 
 Tables.metadata.create_all (engine)
 
