9ba27837bb4091b68342d7369655db807244f9f3
Marcos Dione <mdione@grulic.org.ar>
2016-01-20 17:51:40 +0100

    [+] Trip.fromJson().

--- Elevation/flask/model.py
+++ Elevation/flask/model.py
@@ -4,22 +4,28 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
 from sqlalchemy_utils import ScalarListType
 import itertools
+import json
 
 engine= create_engine ('sqlite:///trip_planner.sqlite3')
 
 Tables= declarative_base ()
 
-# taken from itertools' doc
+# both taken from itertools' doc
 def grouper(iterable, n, fillvalue=None):
     "Collect data into fixed-length chunks or blocks"
     # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
     args = [iter(iterable)] * n
     return itertools.zip_longest(*args, fillvalue=fillvalue)
 
+def flatten(listOfLists):
+    "Flatten one level of nesting"
+    return itertools.chain.from_iterable(listOfLists)
+
 class Trip (Tables):
     __tablename__= 'trips'
 
     id= Column (Integer, primary_key=True)
+    # name= Column (String, primary_key=True)
     name= Column (String)
     points= Column (ScalarListType(float))
 
@@ -28,9 +34,21 @@ class Trip (Tables):
         Notice that the points is converted from a list of floats to alist of tuples of floats.
         Notice too that it is not converted to a string."""
 
+        # convert from [ f, ... ] to [ (x, y), ... ]
         points= [ (x, y) for x, y in grouper (self.points, 2) ]
 
         return { 'name': self.name, 'points': points }
+
+    @classmethod
+    def fromJson (cls, s):
+        """Create a Trip from a string containig its Json repr.
+        Notice that this is not exactly the inverse of toJson(), but serves good enough."""
+        data= json.loads (s)
+
+        # convert from [ (x, y), ... ] to [ f, ... ]
+        points= [ x for x in flatten (data['points']) ]
+
+        return Trip (name=data['name'], points=points)
 
 Tables.metadata.create_all (engine)
 
