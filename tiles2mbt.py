f2ba91050998d3383ba359a1440ae340a6da6ca4
Marcos Dione <mdione@grulic.org.ar>
2016-05-19 18:07:45 +0200

    [+] tool for creating MBTiles files from tilesets.

---
+++ tiles2mbt.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python3
+
+import sys
+import os.path
+
+import map_utils
+
+sector= sys.argv[1]
+
+a= map_utils.Atlas ([sector])
+m= a.maps[sector]
+be= map_utils.MBTilesBackend ("%s.mbt" % sector, m.bbox)
+
+for z in range (m.max_z+1):
+    for x in m.iterate_x (z):
+        for y in m.iterate_y (z):
+            try:
+                data= open (os.path.join ('Elevation', str (z), str (x),
+                                          "%d.png" % y),
+                            'rb').read ()
+            except FileNotFoundError:
+                pass
+            else:
+                be.store (z, x, y, data)
+
+        be.commit ()
